077f8d5264ad95fe959c73746b7763cc42b7e081
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-01-21 22:37:16 -0800

    feat: Implement create query

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,24 +1,33 @@
-import { pool } from '../../database/db/database';
-import { QueryResult } from 'pg';
-import { User } from '../types/user';
+import { pool } from "../../database/db/database";
+import { QueryResult } from "pg";
+import { User } from "../types/user";
+
+function create(user: User): Promise<QueryResult> {
+  const query =
+    "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *;";
+  return pool.query(query, [user.username, user.email, user.passwordHash]);
+}
 
 // Overload function definitions for getting a user by ID or username
 function get(id: number): Promise<QueryResult>; // Function signature for getting a user by ID
 function get(username: string): Promise<QueryResult>; // Function signature for getting a user by username
-function get(identifier: number | string): Promise<QueryResult> { // Implementation of the overloaded get function
-    // Determine the query based on the type of identifier
-    const query = typeof identifier === 'number' ?
-        "SELECT * FROM users WHERE user_id = $1" : // SQL query to select a user by ID
-        "SELECT * FROM users WHERE username = $1"; // SQL query to select a user by username
+function get(identifier: number | string): Promise<QueryResult> {
+  // Implementation of the overloaded get function
+  // Determine the query based on the type of identifier
+  const query =
+    typeof identifier === "number"
+      ? "SELECT * FROM users WHERE user_id = $1" // SQL query to select a user by ID
+      : "SELECT * FROM users WHERE username = $1"; // SQL query to select a user by username
 
-    // Execute the query with the provided identifier
-    return pool.query(query, [identifier]);
+  // Execute the query with the provided identifier
+  return pool.query(query, [identifier]);
 }
 
 // users object containing the get function
 const users = {
-    get // The get function
-}
+  get,
+  create,
+};
 
 // Export the users object as the default export
 export default users;
