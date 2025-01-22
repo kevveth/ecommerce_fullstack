7a4797d4d620058f8aa31d2688b7805c0a1c5fae
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-01-22 01:05:08 -0800

    Implement update query for users

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,11 +1,11 @@
 import { pool } from "../../database/db/database";
 import { QueryResult } from "pg";
-import { User } from "../types/user";
+import { User, UserOptions } from "../types/user";
 
 function create(user: User): Promise<QueryResult> {
   const query =
     "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *;";
-  return pool.query(query, [user.username, user.email, user.passwordHash]);
+  return pool.query(query, [user.username, user.email, user.password]);
 }
 
 // Overload function definitions for getting a user by ID or username
@@ -23,10 +23,63 @@ function get(identifier: number | string): Promise<QueryResult> {
   return pool.query(query, [identifier]);
 }
 
+function update(
+  userId: User["userId"],
+  updatedUser: Partial<Omit<User, "userId" | "password">>
+): Promise<QueryResult> {
+  const { username, email, streetAddress, city, state, zipCode, country } =
+    updatedUser;
+
+  const setClauses: string[] = [];
+  const updateValues: string[] = [];
+
+  if (username) {
+    setClauses.push("username = ?");
+    updateValues.push(username);
+  }
+
+  if (email) {
+    setClauses.push("email = ?");
+    updateValues.push(email);
+  }
+
+  if (streetAddress) {
+    setClauses.push("street_address = ?");
+    updateValues.push(streetAddress);
+  }
+
+  if (city) {
+    setClauses.push("city = ?");
+    updateValues.push(city);
+  }
+
+  if (state) {
+    setClauses.push("state = ?");
+    updateValues.push(state);
+  }
+
+  if (zipCode) {
+    setClauses.push("zip_code = ?");
+    updateValues.push(zipCode);
+  }
+
+  if (country) {
+    setClauses.push("country = ?");
+    updateValues.push(country);
+  }
+
+  const query = `UPDATE users SET ${setClauses.join(
+    ", "
+  )} WHERE user_id = ? RETURNING *`;
+
+  return pool.query(query, [...updateValues, userId]); // Use userId here
+}
+
 // users object containing the get function
 const users = {
   get,
   create,
+  update,
 };
 
 // Export the users object as the default export
--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -2,10 +2,21 @@ export interface User {
   userId?: number; // Optional if it's auto-generated
   username: string;
   email: string;
-  passwordHash: string;
+  password: string;
   streetAddress?: string;
   city?: string;
   state?: string;
   zipCode?: string;
   country?: string;
+}
+
+export enum UserOptions {
+  USERNAME = "username",
+  EMAIL = "email",
+  PASSWORD = "password_hash",
+  STREET_ADDRESS = "street_address",
+  CITY = "city",
+  STATE = "state",
+  ZIP = "zip_code",
+  COUNTRY = "country"
 }
