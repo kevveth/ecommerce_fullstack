94bf91869c0dd22a7bda8ee2bb2d54f517c8c443
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-02 01:00:53 -0800

    feat: Add email uniqueness check to user creation
    Adds a custom validation rule using Zod's `refine` to ensure email uniqueness during user registration.

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { getWithEmail } from "../services/users";
 
 export const userSchema = z.object({
   user_id: z.number().int().optional(),
@@ -13,15 +14,32 @@ export const userSchema = z.object({
 });
 export type User = z.infer<typeof userSchema>;
 
-export const newUserSchema = userSchema.pick({
-  username: true,
-  email: true,
-  password_hash: true,
-});
+export const newUserSchema = userSchema
+  .pick({
+    username: true,
+    email: true,
+  })
+  .extend({
+    password: z.string(),
+  })
+  .refine(
+    async (data) => {
+      const emailExists = await checkEmailExists(data.email);
+      return !emailExists;
+    },
+    {
+      message: "Email already exists",
+      path: ["email"],
+    }
+  );
 export type NewUser = z.infer<typeof newUserSchema>;
 
+async function checkEmailExists(email: string): Promise<Boolean> {
+  const result = await getWithEmail(email);
+  return result.user ? true : false;
+}
 
 export const updateUserSchema = userSchema
   .omit({ user_id: true, password_hash: true })
-  .partial()
+  .partial();
 export type UpdateableUser = z.infer<typeof updateUserSchema>;
