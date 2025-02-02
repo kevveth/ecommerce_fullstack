bf50a0301fac8035212158e535340fc1b02621bd
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-02 01:28:32 -0800

    feat: Prevent runtime errors with Zod environment validation
    Adds Zod schema validation for environment variables to prevent runtime errors caused by incorrect or missing configuration.

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,12 +9,13 @@ import cors, { CorsOptions } from 'cors';
 import apiRoutes from './routes/index';
 import { errorHandler } from './middleware/errors';
 import { initPassport, isAuthenticated } from './middleware/passport.mw';
+import { env } from './utils/env';
 
 const corsOptions: CorsOptions = {
   origin: `http://localhost:5173`,
   optionsSuccessStatus: 200,
 };
-const port = process.env.PORT || 3001;
+const port = env.PORT || 3001;
 
 // Express initialization
 const app = express();
@@ -24,7 +25,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(
   session({
-    secret: "secretkey",
+    secret: env.SECRET_KEY!,
     resave: false,
     saveUninitialized: false,
   })
--- server/src/controller/register.ts
+++ server/src/controller/register.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcrypt";
 import { Request, Response } from "express-serve-static-core";
 import { create, getWithEmail } from "../services/users";
 import BadRequestError from "../errors/BadRequestError";
+import { env } from "../utils/env";
 
 //Handles creating a new user.
 export async function registerUser(req: Request, res: Response) {
@@ -16,7 +17,7 @@ export async function registerUser(req: Request, res: Response) {
   }
 
   // Hash Password using bcrypt
-  const rounds = parseInt(process.env.SALT_ROUNDS as string);
+  const rounds = env.SALT_ROUNDS;
   const salt = await bcrypt.genSalt(rounds);
   const passwordHash = await bcrypt.hash(password, salt);
 
