08cf783d208548c8b6731c05c4a7ace4c7acaf82
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-02 01:54:37 -0800

    refactor: removed validation logic from controller

--- server/src/controller/register.ts
+++ server/src/controller/register.ts
@@ -1,33 +1,22 @@
 import bcrypt from "bcrypt";
 import { Request, Response } from "express-serve-static-core";
-import { create, getWithEmail } from "../services/users";
-import BadRequestError from "../errors/BadRequestError";
+import { create } from "../services/auth/registration";
 import { env } from "../utils/env";
 
 //Handles creating a new user.
 export async function registerUser(req: Request, res: Response) {
   const { username, email, password } = req.body; // Destructure username, email, and password from the request body
 
-  const userAlreadyExists = await getWithEmail(email);
-  if(userAlreadyExists.user) {
-    throw new BadRequestError({
-      message: "User already exists!",
-      logging: true,
-    });
-  }
-
   // Hash Password using bcrypt
   const rounds = env.SALT_ROUNDS;
   const salt = await bcrypt.genSalt(rounds);
   const passwordHash = await bcrypt.hash(password, salt);
 
-  const result = await create({
+  const newUser = await create({
     username,
     email,
     password: passwordHash,
   });
 
-  const { query, user } = result;
-
-  res.status(201).send({ data: user }); // Send the newly created user with 201 status code
+  res.status(201).send({ data: newUser }); // Send the newly created user with 201 status code
 }
