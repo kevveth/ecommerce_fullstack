251c3098086eb1d47e62ab3a088e1ed63d99496b
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-04 02:26:19 -0800

    refactor: improve error handling by parsing incoming values with zod

--- server/src/controller/users.ts
+++ server/src/controller/users.ts
@@ -1,7 +1,4 @@
-import { config } from "dotenv"; // Import environment variables
-config();
-
-import { Request, Response, NextFunction } from "express-serve-static-core";
+import { Request, Response} from "express-serve-static-core";
 import { get, update, remove } from "../services/users";
 import {
   UpdateableUser,
@@ -9,25 +6,25 @@ import {
   User,
   userSchema,
 } from "../models/user.model";
-import BadRequestError from "../errors/BadRequestError";
 import NotFoundError from "../errors/NotFoundError";
 import z from "zod";
 
-const userIdSchema = z.object({
+const paramSchema = z.object({
   id: z
     .string()
     .regex(/^[1-9]\d*$/) // Matches positive integers only
-    .transform((str) => parseInt(str, 10)),
+    .transform((str) => parseInt(str)),
 });
 
 //Handles getting a user by ID
-export async function getUser(req: Request, res: Response, next: NextFunction) {
-  const { id } = userIdSchema.parse(req.params); // Validate and get user ID from request parameters
+export async function getUser(req: Request, res: Response) {
+  const { id } = paramSchema.parse(req.params); // Validate and get user ID from request parameters
 
-  const user: User = await get(id); // Get the user from the database
+  const user = await get(id); // Get the user from the database
+  const { success, data } = userSchema.safeParse(user);
 
   // Check if user exists
-  if (!user) {
+  if (!success) {
     throw new NotFoundError({
       message: "User not found!",
       logging: true,
@@ -35,7 +32,7 @@ export async function getUser(req: Request, res: Response, next: NextFunction) {
     });
   }
 
-  res.status(200).send({ data: user });
+  res.status(200).send({ data });
 }
 
 //Handles updating an existing user
@@ -47,13 +44,14 @@ export async function updateUser(
   >,
   res: Response
 ) {
-  const { id } = userIdSchema.parse(req.params); // Get user ID from request parameters
-  
+  const { id } = paramSchema.parse(req.params); // Get user ID from request parameters
+
   // Get update data from request body
   const updates = updateUserSchema.parse(req.body);
   const user: User = await update(id, updates); // Update the user in the database
+  const { success, data } = userSchema.safeParse(user);
 
-  if (!user) {
+  if (!success) {
     throw new NotFoundError({
       message: "User not found!",
       logging: true,
@@ -61,34 +59,23 @@ export async function updateUser(
     });
   }
 
-  res.status(200).send({ data: user }); // Send updated user data with 200 status code
+  res.status(200).send({ data }); // Send updated user data with 200 status code
 }
 
 //Handles deleting a user
-export async function deleteUser(
-  req: Request<{ id: number }>,
-  res: Response
-): Promise<void> {
-  const { id } = req.params; // Get user ID from request parameters
-
-  if (!id) {
-    // Check if ID is present
-    throw new BadRequestError({
-      message: "ID is required",
-      logging: true,
-    });
-  }
+export async function deleteUser(req: Request, res: Response) {
+  const { id } = paramSchema.parse(req.params); // Get user ID from request parameters
 
   const user = await remove(id); // Delete the user from the database
+  const { success, data } = userSchema.safeParse(user);
 
-  if (!user) {
-    // Check if user exists
+  if (!success) {
     throw new NotFoundError({
       message: "User not found!",
       logging: true,
-      context: { path: "/users/:id", id, method: "DELETE" },
+      context: { method: "GET", path: "/users/:id", id },
     });
   }
 
-  res.status(204).send({ data: user }); // Send empty response with 204 status code
+  res.status(204).send({ data });
 }
