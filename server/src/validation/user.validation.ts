852f3913b8967e876cf4bed28b8ac7a26f9e74c5
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-05 18:59:38 -0800

    refactor: Moved update validation to user.validation file to clean up update controller. Handling missing users from db request with custom error handler instead of Zod, which should be used for validation only.

--- server/src/validation/user.validation.ts
+++ server/src/validation/user.validation.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { newUserSchema } from "../models/user.model";
+import { newUserSchema, updateUserSchema } from "../models/user.model";
 
 export const validateRegistrationData = async (
   req: Request,
@@ -7,6 +7,19 @@ export const validateRegistrationData = async (
   next: NextFunction
 ) => {
   const { success, error } = await newUserSchema.spa(req.body);
+  if ( success ) {
+    next();
+  } else {
+    res.status(400).send(error.errors);
+  }
+};
+
+export const validateUpdateData = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { success, error } = updateUserSchema.safeParse(req.body);
   if ( success ) {
     next();
   } else {
