2dc6872e745d7538e4cbb94bf8f52fdb5f4ac84a
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-09 14:40:48 -0800

    Updated default pages and file structure

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import { PageLayout } from "../components/PageLayout";
 
 export function Home() {
   return (
-    <div>
+    <PageLayout>
+      <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the default home page.</p>
     </div>
+    </PageLayout>
   );
 };
 
