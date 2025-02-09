25762b59398a8180a5bb0581f8a832050602a321
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-09 14:42:52 -0800

    Added navigation and profile page

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from './styles.module.css'
+
+export const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ client/src/components/ProfileData.tsx
@@ -0,0 +1,11 @@
+type ProfileDataProps = {
+  data: any;
+};
+
+export function ProfileData({ data }: ProfileDataProps) {
+  return (
+    <>
+      <p>This is the profile page.</p>
+    </>
+  );
+}
