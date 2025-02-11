81d3e2a6a18aedc50710b407c387e1aa400e6790
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-10 22:43:45 -0800

    Updated user routes to retrive all users, and a user by username

--- server/src/routes/usersRoute.ts
+++ server/src/routes/usersRoute.ts
@@ -1,14 +1,18 @@
 import express, { Router } from "express";
 import {
   deleteUser,
+  getAllUsers,
   getUser,
+  getUserByUsername,
   updateUser,
 } from "../controller/users";
 import { validateUpdateData } from "../validation/user.validation";
 
 const router: Router = express.Router();
 
-router.get("/:id", getUser);
+// router.get("/:id", getUser);
+router.get('/', getAllUsers);
+router.get("/:username", getUserByUsername);
 router.put("/:id", validateUpdateData, updateUser);
 router.delete("/:id", deleteUser);
 
