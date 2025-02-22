aead931e95f60576dee5695f655dd5202bb130ad
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-22 00:57:26 -0800

    Refatored page layout and routing format

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -8,6 +8,7 @@ import Login from "../pages/Login/Login";
 import { NotFound } from "../errors/NotFound";
 import { Profile } from "../pages/Profile";
 import { Profiles } from "../pages/Profiles";
+import { PageLayout } from "./PageLayout";
 
 export const Router: React.FC = () => {
   const location = useLocation();
@@ -15,12 +16,19 @@ export const Router: React.FC = () => {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profiles" element={<Profiles />} />
-      <Route path="/profiles/:username" element={<Profile />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<PageLayout />}>
+        {/* public routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* protected routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/profiles" element={<Profiles />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+
+        {/* catch all */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
     </Routes>
   );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,8 @@
-import { PageLayout } from "../components/PageLayout";
-
 export function Home() {
   return (
-    <PageLayout>
-      <div>
+    <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the default home page.</p>
     </div>
-    </PageLayout>
   );
-};
-
+}
