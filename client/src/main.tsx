ada8baa2e55a58225b2bfc67f28464cc4e4e3660
Kenneth Rathbun <68872850+kevveth@users.noreply.github.com>
2025-02-26 01:13:27 -0800

    Implemented Snackbar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,15 +4,17 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SnackbarProvider } from "notistack";
 
 const queryClient = new QueryClient();
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
