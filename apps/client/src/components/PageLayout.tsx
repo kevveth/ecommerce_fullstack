b31ebc9bd2e4f13a801b819bd980503aadbf6d83
kevveth <68872850+kevveth@users.noreply.github.com>
2025-03-23 14:58:35 -0700

    Implemented turbo to build repos faster, and removed PageLayout from various pages.

--- apps/client/src/components/PageLayout.tsx
+++ apps/client/src/components/PageLayout.tsx
@@ -1,8 +1,6 @@
-import { PropsWithChildren } from "react";
 import { Navbar } from "./Navbar/Navbar";
 import { Outlet } from "react-router";
 
-type PageLayoutProps = PropsWithChildren;
 
 export function PageLayout() {
   return (<div className="">
