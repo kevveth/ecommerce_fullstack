5b964b73444a03975f8a7f35f3ce4809bff66f85
kevveth <68872850+kevveth@users.noreply.github.com>
2025-04-04 01:48:51 -0700

    Fixed issue with validating the request body. Replaced parse() with parseAsync() due to the new user schema containing an async refinement.

--- apps/server/src/controller/registerController.ts
+++ apps/server/src/controller/registerController.ts
@@ -3,22 +3,32 @@ import { Request, Response } from "express";
 import { create } from "../services/auth/registration";
 import { env } from "../utils/env";
 import { newUserSchema } from "../models/user.model";
+import BadRequestError from "../errors/BadRequestError";
 
 //Handles creating a new user.
 export async function registerUser(req: Request, res: Response) {
-  const { username, email, password } = req.body; // Destructure username, email, and password from the request body
+  try {
+    // Validate Input
+    console.log(req.body);
+    const validatedData = await newUserSchema.parseAsync(req.body);
+    console.log(validatedData);
+    const { username, email, password } = validatedData; // Destructure username, email, and password from the request body
 
-  // Hash Password using bcrypt
-  const rounds = env.SALT_ROUNDS;
-  const salt = await bcrypt.genSalt(rounds);
-  const passwordHash = await bcrypt.hash(password, salt);
+    // Hash Password
+    const rounds = env.SALT_ROUNDS;
+    const salt = await bcrypt.genSalt(rounds);
+    const passwordHash = await bcrypt.hash(password, salt);
 
-  const result = await create({
-    username,
-    email,
-    password: passwordHash,
-  });
+    // Call service layer
+    const result = await create({
+      username,
+      email,
+      password: passwordHash,
+    });
 
-  // const user = userSchema.parse(result);
-  res.status(201).send({ data: result }); // Send the newly created user with 201 status code
+    // const user = userSchema.parse(result);
+    res.status(201).send({ data: result }); // Send the newly created user with 201 status code
+  } catch {
+    throw new BadRequestError({ message: "Failed to register new user" });
+  }
 }
