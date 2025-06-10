f3a6c9edb17fd1733502de8a0817f892c74f4fd6
kevveth <68872850+kevveth@users.noreply.github.com>
2025-06-10 02:34:37 -0700

    fix(server): improve graceful shutdown handling

---
+++ apps/server/src/server.ts
@@ -0,0 +1,66 @@
+import express, { type Express } from "express";
+import "express-async-errors";
+
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth.ts";
+
+import cors, { type CorsOptions } from "cors";
+import { env } from "./utils/env.ts";
+import { errorHandler } from "./middleware/errors.ts";
+import apiRoutes from "./routes/routes.ts";
+import { debug } from "console";
+
+const app: Express = express();
+const port = env.PORT;
+
+const corsOptions: CorsOptions = {
+  origin: env.CLIENT_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+// Auth routes
+app.all("/api/auth/*", toNodeHandler(auth));
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Routes
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/me", async (req, res) => {
+  try {
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+
+    if (!session?.user) {
+      res.status(404).json("User not found");
+    } else {
+      res.status(200).json(session);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get session" });
+  }
+});
+
+app.use("/api", apiRoutes);
+
+// Error handling
+app.use(errorHandler);
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+process.on("SIGINT", () => {
+  debug("SIGINT signal received: closing HTTP server");
+  server.close(() => {
+    debug("HTTP server closed");
+  });
+});
+
+export default app;
